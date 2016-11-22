6f00cd937342267439deaf8ddf73dcfb6729b33d
Aleksandar Nikolić <alexandair@live.com>
2016-11-22 14:15:05 +0100

    Update Get-AzureVMTutorial.ps1

--- Get-AzureVMTutorial.ps1
+++ Get-AzureVMTutorial.ps1
@@ -1,21 +1,4 @@
-
-    #The name of the Automation Credential Asset this runbook will use to authenticate to Azure.
-    $CredentialAssetName = 'DefaultAzureCredential'
-
-    #Get the credential with the above name from the Automation Asset store
-    $Cred = Get-AutomationPSCredential -Name $CredentialAssetName
-    if(!$Cred) {
-        Throw "Could not find an Automation Credential Asset named '${CredentialAssetName}'. Make sure you have created one in this Automation Account."
-    }
-
-    #Connect to your Azure Account
-    $Account = Add-AzureRmAccount -Credential $Cred
-    if(!$Account) {
-        Throw "Could not authenticate to Azure using the credential asset '${CredentialAssetName}'. Make sure the user name and password are correct."
-    }
-
-    #TODO (optional): pick the right subscription to use. Without this line, the default subscription for your Azure Account will be used.
-    #Select-AzureSubscription -SubscriptionName "TODO: your Azure subscription name here"
+$connectionName = "AzureRunAsConnection"try{    # Get the connection "AzureRunAsConnection "    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName             "Logging in to Azure..."    Add-AzureRmAccount `        -ServicePrincipal `        -TenantId $servicePrincipalConnection.TenantId `        -ApplicationId $servicePrincipalConnection.ApplicationId `        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint }catch {    if (!$servicePrincipalConnection)    {        $ErrorMessage = "Connection $connectionName not found."        throw $ErrorMessage    } else{        Write-Error -Message $_.Exception        throw $_.Exception    }}
     
     #Get all the VMs you have in your Azure subscription
     $VMs = Get-AzureRmVM
