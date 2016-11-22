b4cccf39757b74a7ba7b44ae7251b3e22f18150c
Aleksandar Nikolić <alexandair@live.com>
2016-11-22 14:17:03 +0100

    Update Get-AzureVMTutorial.ps1

--- Get-AzureVMTutorial.ps1
+++ Get-AzureVMTutorial.ps1
@@ -1,4 +1,26 @@
-$connectionName = "AzureRunAsConnection"try{    # Get the connection "AzureRunAsConnection "    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName             "Logging in to Azure..."    Add-AzureRmAccount `        -ServicePrincipal `        -TenantId $servicePrincipalConnection.TenantId `        -ApplicationId $servicePrincipalConnection.ApplicationId `        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint }catch {    if (!$servicePrincipalConnection)    {        $ErrorMessage = "Connection $connectionName not found."        throw $ErrorMessage    } else{        Write-Error -Message $_.Exception        throw $_.Exception    }}
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
 
     #Get all the VMs you have in your Azure subscription
     $VMs = Get-AzureRmVM
